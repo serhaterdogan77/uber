f83e262ccdaa54b0974eab33e8f59d8dc2b0ef87
Serhat Erdoğan <serhaterdogan85@outlook.com>
2022-03-13 00:32:25 +0300

    removing styles from index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
 import Header from '../components/Header'
 import HeroSection from '../components/HeroSection'
 import Features from '../components/Features'
